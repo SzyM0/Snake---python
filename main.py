7374bdd5b64d371084abb323105e68e818a4308f
SzyM0 <szymixx12500@gmail.com>
2020-07-13 23:09:47 +0200

    Main menu - cont.
    Three options to choose (EASY, MEDIUM, HARD), "backlight" of the buttons and button class

--- main.py
+++ main.py
@@ -6,11 +6,49 @@ import random
 running = True
 
 
+class Button:
+    def __init__(self, x, y, width, height, color=(0, 0, 255), text="", text_color=(0, 0, 0)):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.text = text
+        self.text_color = text_color
+
+    def draw(self, screen, font_size=0, outline=None):  # Draws a rectanle button with opional outline and text
+        # Makes Outline 2 pixels wide
+        if outline:
+            pygame.draw.rect(screen, outline,
+                             (self.rect.x - 2, self.rect.y - 2, self.rect.width + 4, self.rect.height + 4))
+
+        # Makes Rectangle
+        pygame.draw.rect(screen, self.color, self.rect)
+
+        # This statement puts text in the middle of rectangle
+        if self.text != "":
+            pygame.font.init()
+            my_font = pygame.font.SysFont('Times New Roman', font_size, bold=True)
+            scrn_text = my_font.render(self.text, False, self.text_color)
+            screen.blit(scrn_text, (self.rect.x + (self.rect.width / 2 - scrn_text.get_width() / 2),
+                                    self.rect.y + (self.rect.height / 2 - scrn_text.get_height() / 2)))
+
+    def isOver(self) -> bool:  # Returns true when mouse is over the button
+        mx, my = pygame.mouse.get_pos()
+        return self.rect.collidepoint(mx, my)
+
+    # When the cursor is above the rectangle it changes color
+    def change_color_when_over(self, current_color, color, screen, font_size=0, outline=None):
+        if self.isOver():
+            self.color = color
+            self.draw(screen, font_size, outline)
+        else:
+            self.color = current_color
+            self.draw(screen, font_size, outline)
+
+
 class Cube:
     rows = 20
     width = 500
 
-    def __init__(self, position, dirx=0, diry=-1, color=(255, 0, 0)):
+    def __init__(self, position: tuple, dirx=0, diry=-1, color=(255, 0, 0)):
         self.pos = position
         self.dirx = dirx
         self.diry = diry
@@ -140,21 +178,16 @@ class Snake:
 
 def random_food(rows: int, item) -> tuple:  # Returns random coordinates after checking if it's not a snake
     # todo - jak grałem to chyba pojawiały się na weżu więc trzeba sprawdzić czy to na pewno dobrze działa
-    positions = item.body[:]
 
     while True:
         x = random.randrange(rows)
         y = random.randrange(rows)
 
-        if (x, y) in positions:
-            continue
-        else:
-            break
-
-    return x, y
+        if (x, y) not in item.body:
+            return x, y
 
 
-def draw_grid(surface, window_size: int, rows: int) -> None:
+def draw_grid(surface, window_size: int, rows: int):
     # Draws grid on the screen.
 
     size_between = window_size // rows
@@ -179,7 +212,7 @@ def ref_scrn(surface, window_size, rows, snake, food) -> None:
     pygame.display.update()
 
 
-def draw_text(screen, text, size, x, y):
+def draw_text(screen, text, size, x, y):  # Draws the text on the screen
     pygame.font.init()
     my_font = pygame.font.SysFont('Times New Roman', size, bold=True)
     scrn_text = my_font.render(text, False, (255, 255, 255))
@@ -196,6 +229,7 @@ def game(screen):
 
     clock = pygame.time.Clock()
     # Game loop
+
     while running:
         pygame.time.delay(50)
         clock.tick(10)
@@ -214,34 +248,75 @@ def game(screen):
 
 
 def main():
-    click = False
+
+    # Making front page of the game - main menu
+    window_size = 500
+    screen = pygame.display.set_mode((window_size, window_size))
+    screen.fill((0, 0, 0))
+    draw_text(screen, "Welcome to", 80, 20, 20)
+    draw_text(screen, "Snake Pro I", 80, 20, 100)
+
+    # Making game button object
+    game_button = Button(125, 250, 250, 70,color=(0, 0, 255), text="Game", text_color=(255, 255, 255))
+    game_button.draw(screen, font_size=60, outline=(255, 255, 0))
+
+    # Making LEVEL BUTTONS
+    level_button_easy = Button(125, 72.5, 250, 70, text="Easy", text_color=(255, 255, 255))
+    level_button_medium = Button(125, 215, 250, 70, text="Medium", text_color=(255, 255, 255))
+    level_button_hard = Button(125, 357.5, 250, 70, text="Hard", text_color=(255, 255, 255))
 
     while running:
 
-        window_size = 500
-        screen = pygame.display.set_mode((window_size, window_size))
-        screen.fill((0, 0, 0))
-        draw_text(screen, "Welcome to", 80, 20, 20)
-        draw_text(screen, "Snake Pro I", 80, 20, 100)
-
-        button_1 = pygame.Rect(125, 250, 250, 70)
-        pygame.draw.rect(screen, (140, 140, 140), button_1)
-        draw_text(screen, "Game", 60, 170, 250)
-        mx, my = pygame.mouse.get_pos()
-
-        if button_1.collidepoint(mx, my):
-            if click:
-                game(screen)
-
-        click = False
-
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    click = True
+            # Changing color if the cursor is over the button
+            if event.type == pygame.MOUSEMOTION:
+                game_button.change_color_when_over((0, 0, 255), (140, 140, 140), screen,
+                                                   font_size=60, outline=(255, 255, 0))
+
+            # If user click the game button he can chose how hard the game should be.
+            # So it shows next page with 3 buttons to chose (EASY, MEDIUM and HARD).
+            # Code down below describes the next page.
+            if event.type == pygame.MOUSEBUTTONDOWN and game_button.isOver():
+
+                screen.fill((0, 0, 0))
+                level_button_easy.draw(screen, font_size=60, outline=(255, 255, 0))
+                level_button_medium.draw(screen, font_size=60, outline=(255, 255, 0))
+                level_button_hard.draw(screen, font_size=60, outline=(255, 255, 0))
+
+                while running:
+
+                    for event_page_2 in pygame.event.get():
+                        if event_page_2.type == pygame.QUIT:
+                            pygame.quit()
+
+                        # Starts the game on EASY mode
+                        if event_page_2.type == pygame.MOUSEBUTTONDOWN and level_button_easy.isOver():
+                            game(screen)
+
+                        # Starts the game on MEDIUM mode
+                        if event_page_2.type == pygame.MOUSEBUTTONDOWN and level_button_medium.isOver():
+                            game(screen)
+
+                        # # Starts the game on HARD mode
+                        if event_page_2.type == pygame.MOUSEBUTTONDOWN and level_button_hard.isOver():
+                            game(screen)
+
+                        # Check if cursor is over the button. If it is it changes the color of the rectangle
+                        if event_page_2.type == pygame.MOUSEMOTION:
+
+                            level_button_easy.change_color_when_over((0, 0, 255), (140, 140, 140), screen,
+                                                                     font_size=60,outline=(255, 255, 0))
+
+                            level_button_medium.change_color_when_over((0, 0, 255), (140, 140, 140), screen,
+                                                                       font_size=60, outline=(255, 255, 0))
+
+                            level_button_hard.change_color_when_over((0, 0, 255), (140, 140, 140), screen,
+                                                                     font_size=60, outline=(255, 255, 0))
+
+                    pygame.display.update()
 
         pygame.display.update()
 
